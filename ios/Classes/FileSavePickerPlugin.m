2f1dae1071ae73e396159141018f12a12c8c2fb3
weifucheng <39686870+weifucheng1102@users.noreply.github.com>
2019-12-04 16:58:22 +0800

    Update FileSavePickerPlugin.m
    删除不必要代码

--- ios/Classes/FileSavePickerPlugin.m
+++ ios/Classes/FileSavePickerPlugin.m
@@ -1,53 +1,25 @@
-#import "FileSavePickerPlugin.h"
+#import "IosVideoConvertPlugin.h"
+#import "VideoChangeTool.h"
 
-@interface FileSavePickerPlugin ()
+@implementation IosVideoConvertPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"ios_video_convert"
+            binaryMessenger:[registrar messenger]];
+  IosVideoConvertPlugin* instance = [[IosVideoConvertPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } if([@"videoConvert" isEqualToString:call.method]){
+      [VideoChangeTool convertMovToMp4FromAVURL:call.arguments[@"path"] andCompeleteHandler:^(NSURL * _Nonnull fileUrl) {
+          result(fileUrl.absoluteString);
+      }];
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
 
 @end
-
-@implementation FLTImagePickerSaverPlugin {
-    FlutterResult _result;
-    NSDictionary *_arguments;
-    UIImagePickerController *_imagePickerController;
-    UIViewController *_viewController;
-}
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
-    FlutterMethodChannel *channel =
-    [FlutterMethodChannel methodChannelWithName:@"save_file"
-                                binaryMessenger:[registrar messenger]];
-    FLTImagePickerSaverPlugin *instance =
-    [[FLTImagePickerSaverPlugin alloc] init];
-    [registrar addMethodCallDelegate:instance channel:channel];
-}
-- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
-    if (_result) {
-        _result([FlutterError errorWithCode:@"multiple_request"
-                                    message:@"Cancelled by a second request"
-                                    details:nil]);
-        _result = nil;
-    }
- if ([@"saveVideo" isEqualToString:call.method]) {
-        _result = result;
-        _arguments = call.arguments;
-        NSLog(@"保存");
-        NSString * videoPath = [_arguments objectForKey:@"path"] ;
-        
-        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(videoPath)) {
-            //保存视频到相簿
-            UISaveVideoAtPathToSavedPhotosAlbum(videoPath, self,
-                   @selector(video:didFinishSavingWithError:contextInfo:), nil);
-            result(@"成功");
-        }
-    }
-    else {
-        result(FlutterMethodNotImplemented);
-    }
-}
-- (void)video:(NSString *)videoPath
-        didFinishSavingWithError:(NSError *)error
-  contextInfo:(void *)contextInfo{
-    
-}
-
-
-@end
-
