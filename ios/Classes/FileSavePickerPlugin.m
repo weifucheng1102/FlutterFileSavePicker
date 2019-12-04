f38653308d2e413e5567065ca14f6c9699368bd1
weifucheng <39686870+weifucheng1102@users.noreply.github.com>
2019-12-04 16:46:39 +0800

    Update FileSavePickerPlugin.m
    ios保存视频

--- ios/Classes/FileSavePickerPlugin.m
+++ ios/Classes/FileSavePickerPlugin.m
@@ -1,15 +1,53 @@
 #import "FileSavePickerPlugin.h"
-#if __has_include(<file_save_picker/file_save_picker-Swift.h>)
-#import <file_save_picker/file_save_picker-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "file_save_picker-Swift.h"
-#endif
 
-@implementation FileSavePickerPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFileSavePickerPlugin registerWithRegistrar:registrar];
-}
+@interface FileSavePickerPlugin ()
+
 @end
+
+@implementation FLTImagePickerSaverPlugin {
+    FlutterResult _result;
+    NSDictionary *_arguments;
+    UIImagePickerController *_imagePickerController;
+    UIViewController *_viewController;
+}
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
+    FlutterMethodChannel *channel =
+    [FlutterMethodChannel methodChannelWithName:@"save_file"
+                                binaryMessenger:[registrar messenger]];
+    FLTImagePickerSaverPlugin *instance =
+    [[FLTImagePickerSaverPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+}
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+    if (_result) {
+        _result([FlutterError errorWithCode:@"multiple_request"
+                                    message:@"Cancelled by a second request"
+                                    details:nil]);
+        _result = nil;
+    }
+ if ([@"saveVideo" isEqualToString:call.method]) {
+        _result = result;
+        _arguments = call.arguments;
+        NSLog(@"保存");
+        NSString * videoPath = [_arguments objectForKey:@"path"] ;
+        
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(videoPath)) {
+            //保存视频到相簿
+            UISaveVideoAtPathToSavedPhotosAlbum(videoPath, self,
+                   @selector(video:didFinishSavingWithError:contextInfo:), nil);
+            result(@"成功");
+        }
+    }
+    else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+- (void)video:(NSString *)videoPath
+        didFinishSavingWithError:(NSError *)error
+  contextInfo:(void *)contextInfo{
+    
+}
+
+
+@end
+
